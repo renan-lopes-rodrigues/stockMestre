b59762a7902518bebef873435fe2a80b1f544be9
Renan Lopes <renan.lopes88@gmail.com>
2024-01-03 15:18:17 -0300

    implemented exception handling

--- main.py
+++ main.py
@@ -3,14 +3,6 @@ from src.warehouse import resources
 from src.warehouse.department.resources import DepartmentResources
 from src.database.data_base_config import engine, SessionLocal
 
-# def get_db():
-#     db = SessionLocal()
-
-#     try:
-#         yield db
-#     finally:
-#         db.close()
-
 app = FastAPI()
 
 app.include_router(resources.router)
--- src/warehouse/department/services.py
+++ src/warehouse/department/services.py
@@ -1,24 +1,39 @@
 from sqlalchemy.orm import Session
 from src.warehouse.models import Department
 from src.warehouse.schemas import ItemBase
+from sqlalchemy.exc import DataError
+from fastapi import HTTPException
 from uuid import uuid4
 
 class DepartmentServices:
 
     @staticmethod
     def create_department(db: Session, department: ItemBase):
-        db_department = Department(name=department.name)
-        db_department.id = uuid4()
-        db_department.description = department.description
-        db.add(db_department)
-        db.commit()
-        db.refresh(db_department)
-        return db_department
+        try:
+            db_department = Department(name=department.name)
+            db_department.id = uuid4()
+            db_department.description = department.description
+            db.add(db_department)
+            db.commit()
+            db.refresh(db_department)
+            return db_department
+        except Exception as ex:
+            raise HTTPException(status_code=500, detail='Internal Error')
 
     @staticmethod
     def get_all_departments(db: Session):
-        return db.query(Department).all()
+        try:
+            return db.query(Department).all()
+        except Exception as ex:
+            raise HTTPException(status_code=500, detail='Internal Error')
 
     @staticmethod
     def get_a_department_by_id(db:Session, department_id: str):
-        return db.query(Department).get(department_id)
+        try:
+            return db.query(Department).get(department_id)
+
+        except DataError as ex:
+            raise HTTPException(status_code=404, detail="Department not found.")
+
+        except Exception as ex:
+            raise HTTPException(status_code=500, detail='Internal Error')
